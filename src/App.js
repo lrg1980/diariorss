a2ef383e7a34daf6be4191582419056c113d9da3
lrg1980 <admin@binweb.net>
2019-03-28 12:40:15 -0300

    Creando componentes Noticias/Noticia

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
+import Header from './componentes/Header';
+import Noticias from './componentes/Noticias';
 
 
 class App extends Component {
+
+  state = {
+    noticias: []
+  }
+  componentDidMount() {
+    this.consultarNoticias();
+  }
+
+  consultarNoticias = () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=ar&category=general&apiKey=b6befcf773db43efae73f7c59d23215b`;
+    
+    fetch(url)
+      .then(respuesta => {
+        return respuesta.json();
+    })
+      .then(noticias => {
+        this.setState({
+          noticias: noticias.articles
+      })
+      //console.log(noticias.articles);
+    })
+  }
+
   render() {
     return (
-      <div className="App">
+      <div className="contenedor-app">
         <Header 
-          titulo = 'Noticias RSS' />
+          titulo='Noticias RSS' />
+        <div className="container white contenedor-noticias">
+          <Noticias
+          noticias = {this.state.noticias} />
+        </div>
       </div>
     );
   }
---
+++ src/componentes/Noticias.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Noticia from './Noticia';
+
+class Noticias extends Component {
+     
+     render() {
+          
+          return ( 
+               <div className="row">
+                    <Noticia />
+               </div>
+           )
+     }
+}
+ 
+export default Noticias;
